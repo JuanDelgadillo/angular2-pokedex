cdfb83636213afee510c25d71c4c689ca112f63b
Juan Delgadillo <j.mnuelh@gmail.com>
2016-03-30 02:03:38 -0430

    Add stats and evolution

--- public/app/components/pokemon.component.ts
+++ public/app/components/pokemon.component.ts
@@ -51,6 +51,54 @@ import {Component} from 'angular2/core'
                   </li>
                 </ul>
               </div>
+
+              <!-- Stats -->
+              <div>
+                <ul class="list-group">
+                  <li class="list-group-item">
+                    <strong>HP</strong>
+                    <span class="badge">{{ pokemon.stats.hp}}</span>
+                  </li>
+                  <li class="list-group-item">
+                    <strong>Attack</strong>
+                    <span class="badge">{{ pokemon.stats.attack}}</span>
+                  </li>
+                  <li class="list-group-item">
+                    <strong>Defense</strong>
+                    <span class="badge">{{ pokemon.stats.defense}}</span>
+                  </li>
+                  <li class="list-group-item">
+                    <strong>Special Attack</strong>
+                    <span class="badge">{{ pokemon.stats['sp.atk']}}</span>
+                  </li>
+                  <li class="list-group-item">
+                    <strong>Special Defense</strong>
+                    <span class="badge">{{ pokemon.stats['sp.def']}}</span>
+                  </li>
+                  <li class="list-group-item">
+                    <strong>Speed</strong>
+                    <span class="badge">{{ pokemon.stats.speed}}</span>
+                  </li>
+                  <li class="list-group-item">
+                    <strong>Total</strong>
+                    <span class="badge">{{ pokemon.stats.total}}</span>
+                  </li>
+                </ul>
+              </div>
+
+              <!-- Evolution -->
+              <div>
+                <div class="text-center" *ngFor="#evolution of pokemon.evolution, #last = last">
+                  <a href="#">
+                    <img src="public/assets/img/pokemons/{{evolution | lowercase}}.jpg" width="160">
+                    <p>
+                      <strong>{{evolution}}</strong>
+                    </p>
+                  </a>
+                  <span *ngIf="!last" class="glyphicon glyphicon-arrow-down"></span>
+                </div>
+              </div>
+
             </div>
           </div>
         </div>
@@ -67,6 +115,16 @@ export class PokemonComponent {
       type: [ "Grass", "Poison" ],
       height: "2′4″ (0.71m)",
       weight: "15.2 lbs (6.9 kg)",
-      abilities: [ "Overgrow", "Chlorophyll"]
+      abilities: [ "Overgrow", "Chlorophyll"],
+      stats: {
+        hp: 45,
+        attack: 49,
+        defense: 49,
+        "sp.atk": 65,
+        "sp.def": 65,
+        speed: 45,
+        total: 318
+      },
+      evolution: [ "Bulbasaur", "Ivysaur", "Venusaur" ]
     }
 }
