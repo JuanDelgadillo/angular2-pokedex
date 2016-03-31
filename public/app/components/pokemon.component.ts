9de3cf4f249b7dde5c8d3953bf31c9b33d34aadd
Juan Delgadillo <j.mnuelh@gmail.com>
2016-03-30 23:44:59 -0430

    Create comments component and add show/hide functionality

--- public/app/components/pokemon.component.ts
+++ public/app/components/pokemon.component.ts
@@ -1,9 +1,11 @@
 import {Component} from 'angular2/core'
 import { ImageifyPipe } from '../pipes/imageify.pipe'
+import { CommentsComponent } from './comments.component'
 
 @Component({
   selector: 'pokemon',
   pipes: [ImageifyPipe],
+  directives:[CommentsComponent],
   template: `
     <div class="container-fluid">
       <!-- pokemon -->
@@ -105,45 +107,7 @@ import { ImageifyPipe } from '../pipes/imageify.pipe'
           </div>
         </div>
       <!-- /pokemon -->
-      <!-- comments -->
-        <button class="btn btn-primary">Show comments <span class="badge">2</span></button>
-        <div class="panel panel-primary">
-          <div class="panel-heading">
-            <h1 class="panel-title">Comments <button class="close pull-right">&times;</button></h1>
-          </div>
-          <div class="panel-body">
-            <form class="form-horizontal" role="form">
-              <div class="form-group">
-                <div class="col-sm-6">
-                  <textarea placeholder="Please tell us what do you think about {{ pokemon.name}}" class="form-control"></textarea>
-                </div>
-                <div class="col-sm-4">
-                  <input type="email" placeholder="Please enter your email address" class="form-control">
-                  <label>
-                    <input id="anonymous" type="checkbox">
-                    Anonymous
-                  </label>
-                </div>
-                <div class="col-sm-2">
-                  <button class="btn btn-lg">Comment</button>
-                </div>
-              </div>
-            </form>
-            <blockquote>
-              <p>OMG Bulbasaur is AMAZING!!!</p>
-              <footer>
-                Comment by <a href="mailto:pepe@pepe.com">pepe@pepe.com</a> on <span>Sep 12, 2014, 11:00:00 PM</span>
-              </footer>
-            </blockquote>
-            <blockquote>
-              <p>Nah, I hate it -_-</p>
-              <footer>
-                Comment by <em>Anonymous</em> on <span>Sep 12, 2014, 11:00:00 PM</span>
-              </footer>
-            </blockquote>
-          </div>
-        </div>
-        <!-- /comments -->
+      <comments [pokemon]="pokemon"></comments>
     </div>
   `
 })
