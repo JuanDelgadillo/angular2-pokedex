4d4544bc26a284fcc6765a5e50dac191ce6fac02
Juan Delgadillo <j.mnuelh@gmail.com>
2016-04-01 07:01:23 -0430

    Add grid and fix css

--- public/app/components/pokedex.component.ts
+++ public/app/components/pokedex.component.ts
@@ -19,14 +19,24 @@ import { PokemonTypeComponent } from './pokemon-type.component'
     			<h1>Pokédex <small class="pull-right">Showing <span class="badge">{{pokemons.length}}</span> Pokémons</small></h1>
 				<h2 *ngIf="pokemonType"><span class="label type-{{pokemonType | lowercase}}">{{pokemonType}}</span></h2>
   			</div>
-			<div class="pokemon" *ngFor="#pokemon of pokemons">
-				<a [routerLink]="['Pokemon', { name: pokemon.name }]">
-					<pokemon-name [pokemon]="pokemon" ></pokemon-name>
-					<pokemon-image [pokemon]="pokemon" ></pokemon-image>
-					<div class="text-center">
-				      <pokemon-type [pokemon]="pokemon"></pokemon-type>
-				    </div>
-				</a>
+			<div class="row" *ngFor="#group of groupped">
+				<div class="col-lg-3" *ngFor="#pokemon of group">
+					<div class="pokemon panel panel-primary">
+						<div class="panel-heading">
+          					<pokemon-name [pokemon]="pokemon" ></pokemon-name>
+        				</div>
+						<div class="panel-body">
+							<a [routerLink]="['Pokemon', { name: pokemon.name }]">
+								<pokemon-image [pokemon]="pokemon" ></pokemon-image>
+							</a>
+						</div>
+						<div class="panel-footer">
+							<div class="text-center">
+								<pokemon-type [pokemon]="pokemon"></pokemon-type>
+							</div>
+        				</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	`
@@ -35,19 +45,30 @@ import { PokemonTypeComponent } from './pokemon-type.component'
 export class PokedexComponent {
 	pokemons = []
 	pokemonType:string
+	groupped
+	
 	constructor (
 		private _pokemonService: PokemonService,
 		private _routeParams: RouteParams) {
 		if (this._routeParams.params.type) {
 			this.pokemonType = this._routeParams.get('type')
-			this._pokemonService.getPokemonsByType(this.pokemonType).subscribe(pokemons => this.pokemons = pokemons)
-			//this._pokemonService.getPokemonsByType(pokemonType)
-			//.subscribe(pokemons => this.pokemons = pokemons)
+			this._pokemonService.getPokemonsByType(this.pokemonType)
+				.subscribe(pokemons => {
+					this.pokemons = pokemons
+					this.groupped = this.partition(pokemons, 4)
+				})
 		} else {
 			this._pokemonService.getPokemons()
-			.subscribe(pokemons => this.pokemons = pokemons)
+			.subscribe(pokemons => {
+				this.pokemons = pokemons
+				this.groupped = this.partition(pokemons, 4)
+			})
 		}
 		
+	}
+	
+	partition = (data, n) => {
+		return _.chain(data).groupBy((element, index) => Math.floor(index / n)).toArray().value()
 	}
 	
 }
