d335970b08e26f9bbd5bd05bfbd080ccd5430f33
Juan Delgadillo <j.mnuelh@gmail.com>
2016-08-29 16:31:23 -0300

    App's route has changed to public/app and also underscore has been added as a node_module dependency instead of a local file.

--- systemjs.config.js
+++ systemjs.config.js
@@ -5,10 +5,11 @@
 (function(global) {
   // map tells the System loader where to look for things
   var map = {
-    'app':                        'app', // 'dist',
+    'app':                        'public/app', // 'dist',
     '@angular':                   'node_modules/@angular',
     'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
-    'rxjs':                       'node_modules/rxjs'
+    'rxjs':                       'node_modules/rxjs',
+    '_':                          'node_modules/underscore/underscore-min.js'
   };
   // packages tells the System loader how to load when no filename and/or no extension
   var packages = {
