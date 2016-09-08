409de0eb76eb37386b2e1d8b239b2dc32f256fe4
Juan Delgadillo <j.mnuelh@gmail.com>
2016-09-07 21:58:06 -0600

    Now all pipes and directives are referenced in a ngModule to be available in the entire app.

--- public/app/app.module.ts
+++ public/app/app.module.ts
@@ -1,15 +1,42 @@
+import { PokemonEvolutionComponent } from './components/pokemon-evolution.component';
+import { PokedexNavbarComponent } from './components/pokedex-navbar.component';
+import { PokemonImageComponent } from './components/pokemon-image.component';
+import { PokemonStatsComponent } from './components/pokemon-stats.component';
+import { PokemonNameComponent } from './components/pokemon-name.component';
+import { PokemonTypeComponent } from './components/pokemon-type.component';
+import { PokemonDataComponent } from './components/pokemon-data.component';
+import { CommentsComponent } from './components/comments.component';
 import { PokedexComponent } from './components/pokedex.component';
 import { PokemonComponent } from './components/pokemon.component';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent }  from './app.component';
-import { NgModule }      from '@angular/core';
+import { ImageifyPipe } from './pipes/imageify.pipe';
+import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { appRouting } from './app.routes'
+import { AppRouting } from './app.routes'
+import { NgModule } from '@angular/core';
 
 @NgModule({
-  imports:      [ BrowserModule, appRouting, FormsModule, HttpModule ],
-  declarations: [ AppComponent, PokedexComponent, PokemonComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    AppRouting,
+    FormsModule,
+    HttpModule
+  ],
+  declarations: [
+    AppComponent,
+    PokedexComponent,
+    PokemonComponent,
+    PokemonEvolutionComponent,
+    PokedexNavbarComponent,
+    PokemonNameComponent,
+    PokemonStatsComponent,
+    PokemonImageComponent,
+    PokemonTypeComponent,
+    PokemonDataComponent,
+    CommentsComponent,
+    ImageifyPipe
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
